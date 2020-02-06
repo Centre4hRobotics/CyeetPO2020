c1bb94a92e2b977a943f745072202c8bb0102eb0
Brayden-Button <bbutton653@gmail.com>
2020-02-06 18:33:08 -0500

    Tidying up
    I have created more detailed comments as well as deleted unneeded parts of the code. As well as spaces so it is easier to look at.

--- Vision/multiCameraServer.py
+++ Vision/multiCameraServer.py
@@ -268,12 +268,10 @@ if __name__ == "__main__":
         startSwitchedCamera(config)
 
     # loop forever
-    #print("Before loop")
     visiontable = NetworkTables.getTable('Vision')
     Recent_Points = []
     while True:
         #print("In loop")
-        # (cv_sink, output_stream, img)
         for index, (cv_sink, output_stream, img) in enumerate(input_output[1:2]):
             time, img  = cv_sink.grabFrame(img)
             
@@ -297,14 +295,14 @@ if __name__ == "__main__":
 
             mask = green_mask - light_mask - oversaturation_mask
 
-            #res = cv2.bitwise_and(img, img, mask=mask1)
-
             #Finding edges
             edged = cv2.Canny(mask, 30, 200)
+
             #getting Countors
             cnts = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
             cnts = imutils.grab_contours(cnts)
             cnts = sorted(cnts, key = cv2.contourArea, reverse = True)
+          
             #Loop through contours
             k = 0
             found_contour = None
@@ -312,69 +310,77 @@ if __name__ == "__main__":
                 k = k+1
                 peri = cv2.arcLength(c, True)
                 approx = cv2.approxPolyDP(c, 0.02 * peri, True)
-                #Draw rectangle
+
+                #Numerical rectangle arround green shape (Used later to find top center)
                 boundRect = cv2.boundingRect(approx)
                 start = (boundRect[0], boundRect[1])
                 end = (boundRect[0] + boundRect[2], boundRect[1] + boundRect[3])
-                #Finding the centerX
+
+                #Finding the centerX (Finding the center of the rectangle [X value])
                 centerX = int((start[0] + end[0])/2)
-                #Bounding rectangle
+
+                #Bounding rectangle (We specify the points of the rectangle)
                 line_point_1_right = (centerX + 20, boundRect[1])
                 line_point_1_left = (centerX - 20, boundRect[1])
                 line_point_2_top = (centerX, boundRect[1] + 20)
                 line_point_2_bottom = (centerX, boundRect[1] - 20)
                 color = (0,0,0)
+
+                #Finding the center point (Center hole)
                 found_contour = (centerX, boundRect)
                 break
-                #Green for 8 sides red for not 8
                 
-                    # screenCnt = approx
-                    # color = (0,255,0)  
-                    # centerX = centerX - 160
-                    
-                    # print("x:", centerX)
-                    # print("y:", boundRect[1])   
-                                
-                # else:
-                    # color = (0,0,255)
-                    # visiontable.putBoolean('Found Contour', False)
-                #Drawing Rectangle and cross arrow
-                # Average[k] = 
-                
-                #Display frame to screen
-                
-            
+            # If there is a Contour countinue
             if found_contour != None:
                 centerX = found_contour[0]
                 boundRect = found_contour[1]
                 color = (0, 255, 0)
+
+                # Defining Edge points of the Rectangle
                 line_point_1_right = (centerX + 20, boundRect[1])
                 line_point_1_left = (centerX - 20, boundRect[1])
                 line_point_2_top = (centerX, boundRect[1] + 20)
                 line_point_2_bottom = (centerX, boundRect[1] - 20)
 
+                # Drawing Rectangle arround shape
                 cv2.rectangle(img, (boundRect[0], boundRect[1]), (boundRect[0]+boundRect[2], boundRect[1]+boundRect[3]), color)
+
+                # Drawing Cross arrow on Center point
                 cv2.line(img, line_point_1_right, line_point_1_left, color, 1)
                 cv2.line(img, line_point_2_top, line_point_2_bottom, color, 1)
+
+                #Line in the middle of the screen
                 cv2.line(img, (160, 240), (160, 0), (128,0,0))
+
+                #Network tables for sending data
                 Recent_Points.append((centerX, boundRect[1]))
                 visiontable.putNumber('XCenter', centerX)
                 visiontable.putNumber('YCenter', boundRect[1])
                 visiontable.putBoolean('Found Contour', True)
+
+            #If no Countors then prevent a error or crash
             else:
                 visiontable.putBoolean('Found Contour', False)
                 Recent_Points.append(None)
+
+            #Every 5 frames Average the center point
             if len(Recent_Points) >= RUNNING_AVERAGE_NUM:
                 num_of_points = len(Recent_Points)
                 average_point = reduce(sum_of_points, Recent_Points)
+
+                #Clear for more data in Recent Points
                 Recent_Points.clear()
+
+                #If the there is points in Average point then begin to Average
                 if average_point != None:
                     average_point = (round(average_point[0] / num_of_points), round(average_point[1] / num_of_points))
                     print("average: ({}, {})".format(average_point[0], average_point[1]))
+
+                    #Draw a cross arrow on the Average
                     cv2.line(img, (average_point[0] + 20, average_point[1]),  (average_point[0] - 20, average_point[1]), (255,0,0), 1)
                     cv2.line(img,(average_point[0], average_point[1] + 20), (average_point[0], average_point[1] - 20) , (255,0,0), 1)
                 
 
-                
+            #Display frame and Image
             output_stream.putFrame(img)
             input_output[index] = (cv_sink, output_stream, img)
